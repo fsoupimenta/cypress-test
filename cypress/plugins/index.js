7789bdbfcff599f2c92843cddafc6407b1d8a44a
Fernando Pimenta <108306295+fsoupimenta@users.noreply.github.com>
2022-12-07 22:18:11 -0300

    Revert "Update index.js"
    This reverts commit e0204b8e17d24b0fe8129a8f313ada90b966e1d3.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,33 +1,19 @@
-/// <reference types="cypress" />
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-require("@cypress/instrument-cra");
-const injectCraDevServer = require("@cypress/react/plugins/react-scripts");
-const installCoverageTask = require("@cypress/code-coverage/task");
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
+///<reference types="cypress" />
 /**
+ * 
  * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+ */require("@cypress/instrument-cra");
+ module.exports = (on, config) => {
+    // https://docs.cypress.io/api/plugins/after-run-api
+    
+    require('@cypress/code-coverage/task')(on, config);
 
-  injectCraDevServer(on, config);
-  installCoverageTask(on, config);
+    // on('after:run', (results) => {
+    //     return require('cypress-sonarqube-reporter/mergeReports')(results, {
+    //     mergeFileName: "sonar.xml"
+    //     })
+    // })
+       
 
-  return config;
+    return config;
 };
